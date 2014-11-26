e188e7a3799f948441d23b2a10c563caa8c070df
Tushar Kapila <tgkprog@gmail.com>
2014-11-26 10:23:05 +0400

    props load

--- ioWriteWithLoggerSimple/src/moh/academic/files/Helper.java
+++ ioWriteWithLoggerSimple/src/moh/academic/files/Helper.java
@@ -1,8 +1,11 @@
 package moh.academic.files;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
@@ -10,13 +13,32 @@ public class Helper {
 	private static final Logger logger = Logger.getLogger(Helper.class);
 	final private SimpleDateFormat sf = new SimpleDateFormat("/yyyy/mm/dd");
 
+	private Properties props = null;
+	
+	Helper(){
+		loadMyProps() ;
+	}
+	
+	private  void loadMyProps() {
+		//InputStream is = null;
+		props = new Properties();
+		try (InputStream is = new BufferedInputStream
+				(Io.class.getResourceAsStream("props.txt"));){
+			
+			props.load(is);
+		} catch(Exception e) {
+			logger.warn("Load res ex " + e, e);
+		}
+		
+	}
 	
 	 File getFolder() {
 		String pid = "w43w";
 		Date dt = new Date();
 
 		String datePart = sf.format(dt);
-		File parent = new File("/apps/dat/" + pid + datePart);
+		String root = props.getProperty("rootSavePath", "/apps/dat/");
+		File parent = new File(root + pid + datePart);
 		logger.debug("Msg parent " + parent);
 		return parent;
 	}
--- ioWriteWithLoggerSimple/src/moh/academic/files/Io.java
+++ ioWriteWithLoggerSimple/src/moh/academic/files/Io.java
@@ -1,18 +1,19 @@
 package moh.academic.files;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.InputStream;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
 public class Io {
 	static {
-
-		System.setProperty("log4j.debug", "true");
-		System.setProperty("log4j.config", "/u/w/w2nps/ApFiles/log4j.xml");
-		printCurrentFolderName();
+		System.out.println("d3");
+		//System.setProperty("log4j.debug", "true");
+		//System.setProperty("log4j.config", "/u/w/w2nps/ApFiles/log4j.xml");
+		//printCurrentFolderName();
 		// LangUtils.sleep(3000);
 
 	}
@@ -33,6 +34,8 @@ public class Io {
 	
 	public final static String NAME = "MOH";
 	
+	
+	
 	private Helper hlp = new Helper();
 	
 	// "/data/ddt/config/ma
@@ -40,17 +43,51 @@ public class Io {
 	public static void main(String[] args) {
 		logger.debug("started " );
 		Io app = new Io();
+		
 		File f = app.saveFile("hi there \n at " + new java.util.Date(), "avi", "my.txt", false);
 		//System.out.println("File saved " + f.getAbsolutePath());
-		
 		
 		logger.info("File saved " + f.getAbsolutePath());
 
 	}
+	
 
 	public File saveFile(String contents, String pathPre, String extn, boolean unique) {
 		PrintWriter ps = null;
 
+		File parent = hlp.getFolder();
+		try {
+			if (!parent.exists()) {
+				parent.mkdirs();
+			}
+			File fileOut = null;
+			if (unique) {
+				fileOut = File.createTempFile(pathPre, extn, parent);
+			} else {
+				fileOut = new File(parent, pathPre + extn);
+			}
+			ps = new PrintWriter(fileOut);
+			ps.write(contents);
+			return fileOut;
+		} catch (Exception e) {
+			System.out.println("Error " + e + " pre " + pathPre);
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (Exception e2) {
+					System.out.println("Error close " + e2);
+
+				}
+			}
+		}
+		return null;
+	}
+	
+	public File saveFile2(String contents, String pathPre, String extn, boolean unique) {
+		PrintWriter ps = null;
+		//try with res
 		File parent = hlp.getFolder();
 		try {
 			if (!parent.exists()) {
