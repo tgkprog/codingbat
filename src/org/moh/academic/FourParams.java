9f75e67fdbe0c9b7af5841f4b8caa7279d7dc00b
Tushar Kapila <tgkprog@gmail.com>
2015-08-21 11:45:03 +0530

    notes

--- src/org/moh/academic/FourParams.java
+++ src/org/moh/academic/FourParams.java
@@ -1,29 +1,33 @@
-package org.moh.academic;
+package academic;
 
 /**
-Target function and test harness
+Target function and, unit test cases and test helper
 */
 public class FourParams {
 
 	public static void main(String[] args) {
+
 		FourParams app = new FourParams();
-		app.testCases();
+		app.fourParamsTestCases();
 
 	}
 
 	private int count;
 	private int errs;
 
-	/** Target function
-	 *
+	/** Target function, this function is the main function to implement. The signature should be same as in your question.
+	can use online tool to test it. In your java file can have system out println but for online test might have to comment that out or wont run.
+	 * Question is omitted - this is a sample.
 	 */
 	public int fourParams(boolean bonus, boolean multiply, int first, int second) {
+		//TODO Real implementation here, this is sample
 		return first + second;
 	}
 
-	/** Test harness call target
+	/** Test helper. This function calls target and checks response - if matches expected.
 
 		Same parameters as target plus the return type - expected value.
+		expected value has to be accurately calculated by developer. Type and number of parameters can change depending on your question.
 	 */
 	void testFourParams(boolean bonus, boolean multiply, int first, int second, int expected) {
 		int actual = 0;
@@ -44,13 +48,14 @@ public class FourParams {
 
 	/*** different test cases - more the better to adequately test the target function for all requirements per the question
 	*/
-	private void testCases() {
+	private void fourParamsTestCases() {
+		System.out.println("FourParams ");
 		testFourParams(false, false, 1, 1, 2);
 		testFourParams(false, false, 1, 12, 13);
 		testFourParams(false, true, 1, 1, 2);
 		testFourParams(false, true, 5, 3, 15);
-
-		System.out.println("Count " + count + ", Errors :" + errs + ".");
+		testFourParams(true, true, 5, 3, 1);
+		System.out.println("FourParams Count " + count + ", Errors :" + errs + ".");
 	}
 
 }
